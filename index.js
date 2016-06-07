985d53aa01003eb979695e2ac0ffd8304e374646
Becca Barton <hello@beccabarton.com>
2016-06-07 11:11:17 -0400

    first working version of chat app

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var socketIo = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-  res.send('<h1> Cool </h1>');
+app.get('/', function(request, response) {
+  response.sendFile(__dirname + '/index.html');
+});
+
+socketIo.on('connection', function(socket) {
+  console.log('A user is connected!!');
+  socket.on('disconnect', function() {
+    console.log('a user disconnected :(');
+  });
+  socket.on('chat message', function(msg) {
+    socketIo.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function() {
-  console.log('listening on *:3000');
+  console.log('listening on port 3000');
 });
